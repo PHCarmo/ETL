609ee1338286d206681fffd329f7f7bbaa6d20b5
PHCarmo <paulo.henrique.carmo@hotmail.com>
2023-08-25 22:19:07 -0300

    Transform layer

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from extract import Extract
+from transform import Transform
 
 if __name__ == '__main__':
     e = Extract("public_ips.csv")
     ips = e.get_file_data()
     users = e.get_users(ips)
+
+    t = Transform()
+    t.set_suggestions(users)
---
+++ transform.py
@@ -0,0 +1,27 @@
+import requests
+
+class Transform:
+    def __init__(self):
+        self.HOST = "https://www.googleapis.com/customsearch/v1"
+        self.ACCESS_KEY = "Insert your API key here"
+        self.CONTEXT = "51275c3de2d644c29"
+
+    def set_suggestions(self, users):
+        for user in users:
+            if (results := self.get_google_results(user)):
+                suggestions = {}
+                for item in results["items"]:
+                    suggestions[item["title"]] = item["link"]
+                user.suggestions = suggestions
+
+    def get_google_results(self, user):
+        params = {
+            "key": self.ACCESS_KEY,
+            "cx": self.CONTEXT,
+            "q": f"Things to do in a {user.get_weather()} day in {user.get_location()}",
+        }
+        response = requests.get(
+            f'{self.HOST}',
+            params=params
+        )
+        return response.json() if response.status_code == 200 else None
